5578a1e164ddd959b940d2c0f7497185c68c1bcd
34721 <basavaraj.s2@tataelxsi.co.in>
2023-01-02 14:42:33 +0530

    Changes in environment variable

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const non_auth_routers = require("./routes/no-auth.routes");
 const itemsRoutes = require("./routes/items.routes");
 const orderRoutes = require("./routes/orders.routes");
 const socketIO = require("socket.io");
+const http = require("http");
 require("dotenv").config();
 require("./config/dbcon");
 
@@ -19,10 +20,30 @@ app.use("/v1/api/non-auth", non_auth_routers);
 app.use("/v1/api/items", itemsRoutes);
 app.use("/v1/api/orders", orderRoutes);
 
-const io = socketIO(
-  app.listen(PORT, () => {
-    console.log("Triparva application started in port number", PORT);
-  })
-);
+const server = http.createServer(app);
 
-const getApiAndEmit = "TODO";
+const io = socketIO(server);
+
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});
+
+const getApiAndEmit = (socket) => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+server.listen(PORT, () => {
+  console.log("Triparva application started in port number", PORT);
+});
