a33399613450fac4c6950863c2730952f7f1edd7
34721 <basavaraj.s2@tataelxsi.co.in>
2023-01-02 15:37:28 +0530

    Socket connect establishment

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const non_auth_routers = require('./routes/no-auth.routes');
-const itemsRoutes = require('./routes/items.routes');
-const orderRoutes = require('./routes/orders.routes');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const non_auth_routers = require("./routes/no-auth.routes");
+const itemsRoutes = require("./routes/items.routes");
+const orderRoutes = require("./routes/orders.routes");
+
+// Socket server
+const socketIO = require("socket.io");
+const http = require("http");
+
 require("dotenv").config();
-require('./config/dbcon');
+require("./config/dbcon");
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,10 +19,20 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/v1/api/non-auth', non_auth_routers);
-app.use('/v1/api/items', itemsRoutes);
-app.use('/v1/api/orders', orderRoutes);
+app.use("/v1/api/non-auth", non_auth_routers);
+app.use("/v1/api/items", itemsRoutes);
+app.use("/v1/api/orders", orderRoutes);
 
-app.listen(PORT, () => {
-    console.log("Triparva application started in port number", PORT);
+// Create server
+const server = http.createServer(app);
+
+// IO socket connect
+const io = new socketIO(server, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
+});
+
+server.listen(PORT, () => {
+  console.log("Triparva application started in port number", PORT);
 });
