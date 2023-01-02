83f41ac78fbf1ee85557f3706dd609df3c9b2674
34721 <basavaraj.s2@tataelxsi.co.in>
2023-01-02 14:20:49 +0530

    Changes in environment variable

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const non_auth_routers = require('./routes/no-auth.routes');
-const itemsRoutes = require('./routes/items.routes');
-const orderRoutes = require('./routes/orders.routes');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const non_auth_routers = require("./routes/no-auth.routes");
+const itemsRoutes = require("./routes/items.routes");
+const orderRoutes = require("./routes/orders.routes");
+const socketIO = require("socket.io");
 require("dotenv").config();
-require('./config/dbcon');
+require("./config/dbcon");
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,10 +15,14 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/v1/api/non-auth', non_auth_routers);
-app.use('/v1/api/items', itemsRoutes);
-app.use('/v1/api/orders', orderRoutes);
+app.use("/v1/api/non-auth", non_auth_routers);
+app.use("/v1/api/items", itemsRoutes);
+app.use("/v1/api/orders", orderRoutes);
 
-app.listen(PORT, () => {
+const io = socketIO(
+  app.listen(PORT, () => {
     console.log("Triparva application started in port number", PORT);
-});
+  })
+);
+
+const getApiAndEmit = "TODO";
