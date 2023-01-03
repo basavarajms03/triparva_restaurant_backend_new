8b12fffb68c42d21f5a6cb613f0d724a354d4f61
34721 <basavaraj.s2@tataelxsi.co.in>
2023-01-03 10:55:43 +0530

    Socket old version

--- index.js
+++ index.js
@@ -30,10 +30,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"],
-    transports: ["websocket", "polling"],
   },
-  allowEIO3: true,
 });
 
 io.on("connection", (socket) => {
